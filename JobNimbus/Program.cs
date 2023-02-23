36b2768dffb5adb0666a4f1ea27d316494907c26
peter <pete.zabala@gmail.com>
2023-02-23 15:51:41 +0800

    PSZ: JobNumbus test

--- JobNimbus/Program.cs
+++ JobNimbus/Program.cs
@@ -10,6 +10,34 @@ namespace JobNimbus
     {
         static void Main(string[] args)
         {
+            string param = "";
+            Console.Write("Enter string:");
+            param = Console.ReadLine();
+
+            var result = checkBracket(param);
+
+            Console.Write($"Result is: {result}");
+
+            Console.ReadLine();
+        }
+
+        public static bool checkBracket(string param)
+        {
+            //PSZ: HONEST Goggled
+            //  *best way to iterate in string
+            int isBalanced = 0;
+            foreach (char c in param)
+            {
+                if (c == '{')
+                    isBalanced++;
+                if (c == '}')
+                    isBalanced--;
+
+                if (isBalanced < 0)
+                    break;
+
+            }
+            return isBalanced == 0;
         }
     }
 }
